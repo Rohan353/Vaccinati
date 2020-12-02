c64a65929145e63794291ed0eef79a60b2a09c39
Rohan353 <rohanwoodcock@gmail.com>
2020-12-02 20:10:46 +0400

    pls may this work

--- Welcome.py
+++ Welcome.py
@@ -1,18 +1,28 @@
 print('DIDI 2020')
 print('test')
 
+
 def rohan353():
-  x = int(input("coolman"))
-  if x == 15:
-    print("excellent")
-  if x != 15:
-    print("not cool man")
+    x = int(input("coolman"))
+    if x == 15:
+        print("excellent")
+    if x != 15:
+        print("not cool man")
+
+
 rohan353()
 
+
 def mike():
-  y = input("block")
-  if y == "chain":
-    print("https: // github.com / Rohan353 / DIDI - 2020.git")
-  else:
-    print("wrong")
+    y = input("block")
+    if y == "chain":
+        print("https: // github.com / Rohan353 / DIDI - 2020.git")
+    else:
+        print("wrong")
+
+
 mike()
+
+
+def test():
+    print('if this works i am happy')
