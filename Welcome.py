94f69e0c401d48a79d1889e17be28f759447a65e
Mike Rossberg <perlysenderskt@gmail.com>
2020-12-02 19:51:37 +0400

    Added new func called mike

--- Welcome.py
+++ Welcome.py
@@ -8,3 +8,11 @@ def rohan353():
   if x != 15:
     print("not cool man")
 rohan353()
+
+def mike():
+  y = input("block")
+  if y == "chain":
+    print(https: // github.com / Rohan353 / DIDI - 2020.
+    git)
+  else:
+    print("wrong")
